3e63b23cbe883ffc799336aa03178ded46141644
diegobarr <d1390xd.v@gmail.com>
2025-08-24 16:19:13 -0600

    Codigo base terminado, junto con menu de opciones (pendiente impresion de datos)

--- sistemaEncuestas.py
+++ sistemaEncuestas.py
@@ -1,52 +1,75 @@
 encuestas={}
 
 class CreateSurvey:
-    def __init__(self):
+    def __init__(self, nombre):
+        self.nombre = nombre
         self.questionList =[]
 
     def agregar_question(self, question):
         self.questionList.append(question)
 
-class register_request(CreateSurvey): #(CreateSurvey)
-    def __init__(self):
-        super().__init__()
+class registerRequest(CreateSurvey): #(CreateSurvey)
+    def __init__(self, nombre):
+        super().__init__(nombre)
         self.respuestas={}
 
     def registrar_respuestas(self):
-        for i, value in enumerate(encuestas.items(), start=1):
-            print(f"Encuentas #{i}. {value[0]}")
+        print(f"Encuesta {self.nombre}")
 
-        escojer = input("Escoja una encuesta por su nombre: ").lower()
-
-        if escojer in encuestas:
-            for i, pregunta in enumerate(self.questionList, start=1):
-                respuesta=input(f"{i}. {pregunta}: ")
-                self.respuestas[pregunta] = respuesta
-            encuestas[escojer]=self.respuestas
-
-        else:
-            print("La encuesta no existe.")
+        for i, pregunta in enumerate(self.questionList, start=1):
+            respuesta=input(f"{i}. {pregunta}: ")
+            self.respuestas[pregunta] = respuesta
 
     def mostrar_respuestas(self):
         for i, (pregunta, valor) in enumerate(self.respuestas.items(), start=1):
             print(f"Pregunta {i}: {pregunta}")
             print(f"Respuesta: {valor}\n")
 
+while True:
+    print("Bienvenido al sistema Creación de Encuestas Dinámicas")
+    print("1. Crear una encuesta")
+    print("2. Registrar responder una encuesta")
+    print("3. Mostrar respuestas de la encuesta a escojer")
+    print("4. Salir")
+
+    option=input("Seleccione una opciones: ")
+
+    match option:
+        case "1":
+            num_encuesta=int(input("Ingrese el numero de encuestas: "))
+
+            for i in range(num_encuesta):
+                nombre = input("Ingrese el nombre de la encuesta: ").lower()
+                cantidad = int(input("Ingrese la cantidad de preguntas a ingresar: "))
+
+                encuesta = registerRequest(nombre)
+
+                for i in range(cantidad):
+                    variable = input(f"Ingrese la pregunta {i + 1}: ")
+                    encuesta.agregar_question(variable)
+
+                encuestas[nombre] = encuesta
+            print("Encuestas registradas")
+
+        case "2":
+            escojer=input("Ingrese el nombre de la encuesta que desee responder: ").lower()
+
+            encuestas[escojer].registrar_respuestas()
+
+        case "3":
+            pass
+
+        case "4":
+            print("Saliendo...")
+
+        case _:
+            print("Opción no válida")
 
 
-num_encuesta=int(input("Ingrese el numero de encuestas: "))
-respuesta = register_request()#Instanciamiento de objeto, donde se toma tanto la lista de preguntas como de respuestas
 
-for i in range(num_encuesta):
-    nombre=input("Ingrese el nombre de la encuesta: ").lower()
-    cantidad=int(input("Ingrese la cantidad de preguntas a ingresar: "))
 
-    for i in range(cantidad):
-        variable=input(f"Ingrese la pregunta {i+1}: ")
-        respuesta.agregar_question(variable)
-    encuestas[nombre]=respuesta
 
-respuesta.registrar_respuestas()
+
 
 
 
