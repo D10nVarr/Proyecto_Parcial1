729eb0d9ec785dde1b21a55c1326a8204f53b569
diegobarr <d1390xd.v@gmail.com>
2025-08-24 11:36:51 -0600

    Arreglos de compatibilidad entre clases

--- sistemaEncuestas.py
+++ sistemaEncuestas.py
@@ -1,18 +1,17 @@
-questionList = ["¿Si o no?", "¿Te gusta la comida?", "¿Cuánto pesas?"]
-
 class CreateSurvey:
-    def __init__(self, question):
-        self.question = question
-        super().__init__()
-        questionList.append(self)
+    def __init__(self):
+        self.questionList =[]
 
-class register_request: #(CreateSurvey)
+    def agregar_question(self, question):
+        self.questionList.append(question)
+
+class register_request(CreateSurvey): #(CreateSurvey)
     def __init__(self):
         super().__init__()
         self.respuestas={}
 
     def registrar_repuestas(self):
-        for i, pregunta in enumerate(questionList, start=1):
+        for i, pregunta in enumerate(self.questionList, start=1):
             respuesta=input(f"{i}. {pregunta}: ")
             self.respuestas[pregunta] = respuesta
 
@@ -20,8 +19,14 @@ class register_request: #(CreateSurvey)
         for i, valor in enumerate(self.respuestas.values(), start=1):
             print(f"Pregunta {i}: {valor}")
 
-objeto=register_request()
 
-objeto.registrar_repuestas()
 
-objeto.mostrar_respuestas()
+
+##Pruebas de compatibilidad/posible forma de manejar el ingreso de datos
+respuesta = register_request()#Instanciamiento de objeto, donde se toma tanto la lista de preguntas como de respuestas
+for i in range(3):
+    si=input(f"Ingrese el pregunta {i+1}: ")
+    respuesta.agregar_question(si)
+
+respuesta.registrar_repuestas()
+respuesta.mostrar_respuestas()
