d4b920bd577f0f56a25c6fd7ca93448c88522e56
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-08-24 15:34:19 -0600

    escojer pregunta con excepcion

--- sistemaEncuestas.py
+++ sistemaEncuestas.py
@@ -10,7 +10,7 @@ class register_request(CreateSurvey):
         super().__init__()
         self.respuestas={}
 
-    def registrar_repuestas(self):
+    def registrar_respuestas(self):
         if not self.questionList:
             raise ValueError("No hay preguntas registradas aún.")
         for i, pregunta in enumerate (self.questionList, start=1):
@@ -23,25 +23,44 @@ class register_request(CreateSurvey):
 
     def mostrar_respuestas(self):
         if not self.respuestas:
-            print("No hay respiestas registradas todavia.")
+            print("No hay respuestas registradas todavia.")
             return
         for i, (pregunta,valor) in enumerate(self.respuestas.items(), start=1):
             print(f"Pregunta {i}: {pregunta}")
             print(f"Respuesta: {valor}\n")
-titul_encuesta = input("Ingrese el titulo de la encuesta: ")
-respuesta = register_request(titul_encuesta)
+encuestas={}
 
-while True:
-    cantidad = input("Ingrese la cantidad de preguntas: ")
-    if cantidad <= 0:
-        print("las preguntas deben ser mayor a 0.")
-        continue
-
-    for i range(cantidad)
-        pregunta = input(f"Ingrese la pregunta {i + 1}")
+num_encuesta=int(input("Ingrese el numero de encuestas: "))
 
 
+for i in range(num_encuesta):
+    nombre=input("Ingrese el nombre de la encuesta: ")
+    ##Pruebas de compatibilidad/posible forma de manejar el ingreso de datos
+    while True:
+        try:
+            cantidad =int(input("Ingrese la cantidad de preguntas a ingresar: "))
+            if cantidad <=0:
+                print("Debe ingresar un numero mayor a 0.")
+                continue
+            break
+        except ValueError:
+            print("Por favor ingrese un numero valido.")
+    respuesta = register_request()
 
+    for i in range(cantidad):
+        variable=input(f"Ingrese la pregunta {i+1}: ")
+        respuesta.agregar_question(variable)
 
-respuesta.registrar_repuestas()
+    encuestas[nombre]=respuesta
+escojer = input("Escoja una encuesta: ")
+if escojer in encuestas:
+    encuestas[escojer].registrar_respuestas()
+else:
+    print("La encuesta no existe.")
+
+for nombre,valor in encuestas.items():
+    print(f"\nEncuestas: {nombre}")
+    valor.mostrar_respuestas()
+
+respuesta.registrar_respuestas()
 respuesta.mostrar_respuestas()
