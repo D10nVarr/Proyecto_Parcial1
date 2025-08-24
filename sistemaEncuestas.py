6e1224b7b954ff3a9c670a9baa7d29d2f8e39412
diegobarr <d1390xd.v@gmail.com>
2025-08-23 23:40:31 -0600

    Para que lo vea Melqui

--- sistemaEncuestas.py
+++ sistemaEncuestas.py
@@ -1,2 +1,21 @@
-print("commit inicial")
-print("")
+questionList = []
+
+class CreateSurvey:
+    def __init__(self, question):
+        self.question = question
+        super().__init__()
+        questionList.append(self)
+
+class register_request: #(CreateSurvey)
+    def __init__(self):
+        super().__init__()
+        self.respuestas={}
+
+    def registrar_repuestas(self):
+        for i, pregunta in enumerate(questionList, start=1):
+            respuesta=input(f"{i}. {pregunta}: ")
+            self.respuestas[pregunta] = respuesta
+
+    def mostrar_respuestas(self):
+        for i, valor in enumerate(self.respuestas.values(), start=1):
+            print(f"Pregunta {i}: {valor}")
