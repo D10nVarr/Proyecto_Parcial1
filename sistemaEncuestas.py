7c733cff16e93740a423bd9f7288d09f22650e4d
José Noguera <juniornoguera1@gmail.com>
2025-08-23 22:02:39 -0600

    Creador de encuesta

--- sistemaEncuestas.py
+++ sistemaEncuestas.py
@@ -1,5 +1,11 @@
 print("commit inicial")
 
 class CreateSurvey:
-    def __init__(self):
-        print("Commit para agregar a github")
+    def __init__(self, question):
+        self.question = question
+        super().__init__()
+
+x = input("Ingrese la pregunta: ")
+y = CreateSurvey(x)
+print(y)
+print(y.question)
