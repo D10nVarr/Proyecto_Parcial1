9ad4d0d5b79202bee105d72daef911899440cfe1
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-08-24 12:37:58 -0600

    mostrar resultado iniciado, raise valuerror agregado

--- sistemaEncuestas.py
+++ sistemaEncuestas.py
@@ -5,17 +5,37 @@ class CreateSurvey:
         self.question = question
         super().__init__()
         questionList.append(self)
+    def __str__(self):
+        return f"Pregunta: {self.question}"
 
 class register_request: #(CreateSurvey)
     def __init__(self):
-        super().__init__()
         self.respuestas={}
 
     def registrar_repuestas(self):
+        if not questionList:
+            raise ValueError("No hay preguntas registradas aún.")
         for i, pregunta in enumerate(questionList, start=1):
-            respuesta=input(f"{i}. {pregunta}: ")
+            respuesta= input(f"{i}. {pregunta}: ").strip()
+            if not respuesta:
+                print("Se registrara como 'Sin respuesta'.")
+                respuesta = "Sin respuesta"
             self.respuestas[pregunta] = respuesta
+        print("Respuestas registradas correctamente")
 
     def mostrar_respuestas(self):
-        for i, valor in enumerate(self.respuestas.values(), start=1):
-            print(f"Pregunta {i}: {valor}")
+        if not self.respuestas
+            print("No hay respiestas registradas todavia.")
+            return
+        for i, (pregunta,valor) in enumerate(self.respuestas.values(), start=1):
+            print(f"Pregunta {i}: {pregunta}")
+            print(f"Respuesta: {valor}")
+
+class SistemaEncuestas:
+    def __init__(self):
+        self.respuestas_guardadas = []
+
+    def crear_encuesta(self):
+
+
+
