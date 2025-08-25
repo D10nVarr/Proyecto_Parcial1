23f5c64b18ac822c68cdc3ab00861e4c292a06c9
Alejandro <alejandrocoxaj94@gmail.com>
2025-08-24 18:29:15 -0600

    Alejandro hizo las condiciones de case 1.

--- sistemaEncuestas.py
+++ sistemaEncuestas.py
@@ -1,15 +1,84 @@
-print("commit inicial")
-
-questionList = []
+encuestas={}
 
 class CreateSurvey:
-    def __init__(self, question):
-        self.question = question
-        super().__init__()
-        questionList.append(self)
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.questionList =[]
 
-x = input("Ingrese la pregunta: ")
-y = CreateSurvey(x)
-print(y)
-print(y.question)
-print(questionList)
+    def agregar_question(self, question):
+        self.questionList.append(question)
+
+class registerRequest(CreateSurvey): #(CreateSurvey)
+    def __init__(self, nombre):
+        super().__init__(nombre)
+        self.respuestas={}
+
+    def registrar_respuestas(self):
+        print(f"Encuesta {self.nombre}")
+
+        for i, pregunta in enumerate(self.questionList, start=1):
+            respuesta=input(f"{i}. {pregunta}: ")
+            self.respuestas[pregunta] = respuesta
+
+    def mostrar_respuestas(self):
+        for i, (pregunta, valor) in enumerate(self.respuestas.items(), start=1):
+            print(f"Pregunta {i}: {pregunta}")
+            print(f"Respuesta: {valor}\n")
+
+while True:
+    print("\nBienvenido al sistema de Encuestas Dinámicas")
+    print("1. Crear una encuesta")
+    print("2. Responder una encuesta")
+    print("3. Mostrar respuestas de la encuesta a escojer")
+    print("4. Salir")
+
+    option=input("Selecciona una opción (1-4): ")
+
+    match option:
+        case "1":
+            num_encuesta=int(input("Ingrese el numero de encuestas: "))
+            if num_encuesta == 0:
+                print("Número inválido.")
+
+            else:
+                num_encuesta = int(num_encuesta)
+                for i in range(num_encuesta):
+                    nombre = input("Ingrese el nombre de la encuesta: ").lower()
+                    if nombre.isdigit() or nombre == "":
+                        print("Nombre inválido. Debe contener letras.")
+                    else:
+                        cantidad = input("Ingrese la cantidad de preguntas a ingresar: ")
+                        if not cantidad.isdigit() or int(cantidad) <= 0:
+                            print("Número de preguntas inválido. Debe ser mayor que 0.")
+                        else:
+                            cantidad = int(cantidad)
+                            encuesta = registerRequest(nombre)
+                            for j in range(cantidad):
+                                pregunta = input(f"Ingrese la pregunta {j + 1}: ")
+                                encuesta.agregar_question(pregunta)
+                            encuestas[nombre] = encuesta
+                            print("Encuestas registradas.")
+
+        case "2":
+            escojer=input("Ingrese el nombre de la encuesta que desee responder: ").lower()
+
+            encuestas[escojer].registrar_respuestas()
+
+        case "3":
+            if not encuestas:
+                print("No hay encuestas registradas.")
+            else:
+                print("Encuestas disponibles:")
+                for nombre in encuestas:  # directamente recorre las claves
+                    print(f"- {nombre}")
+                nombre_elegido = input("Ingrese el nombre de la encuesta que desea ver: ").lower()
+                if nombre_elegido in encuestas:
+                    encuestas[nombre_elegido].mostrar_respuestas()
+                else:
+                    print("Encuesta no encontrada.")
+        case "4":
+            print("Saliendo...")
+
+        case _:
+            print("Opción no válida")
+
